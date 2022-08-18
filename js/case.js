129a4a1bf324e128764bcd52f1648b4550e669cf
sufian <sufian4se@gmail.com>
2022-08-18 21:02:51 +0600

    update case number

--- js/case.js
+++ js/case.js
@@ -6,21 +6,26 @@
 5. set the value to the case number field
 */
 
-document.getElementById('btn-case-plus').addEventListener('click', function(){
+function updateCaseNumber(isIncrease) {
+  const caseNumberField = document.getElementById("case-number-field");
+  const caseNumberString = caseNumberField.value;
+  const previousCaseNumber = parseInt(caseNumberString);
 
-    const caseNumberField = document.getElementById('case-number-field');
-    const caseNumberString = caseNumberField.value;
-    const previousCaseNumber = parseInt(caseNumberString);
+  let newCaseNumber;
+  if (isIncrease === true) {
+    newCaseNumber = previousCaseNumber + 1;
+  } else {
+    newCaseNumber = previousCaseNumber - 1;
+  }
+  caseNumberField.value = newCaseNumber;
+}
 
-    const newCaseNumber = previousCaseNumber + 1;
-    caseNumberField.value = newCaseNumber;
+document.getElementById("btn-case-plus").addEventListener("click", function () {
+  updateCaseNumber(true);
 });
 
-document.getElementById('btn-case-minus').addEventListener('click', function(){
-    const caseNumberField = document.getElementById('case-number-field');
-    const caseNumberString = caseNumberField.value;
-    const previousCaseNumber = parseInt(caseNumberString);
-
-    const newCaseNumber = previousCaseNumber - 1;
-    caseNumberField.value = newCaseNumber;
-});
+document
+  .getElementById("btn-case-minus")
+  .addEventListener("click", function () {
+    updateCaseNumber(false);
+  });
