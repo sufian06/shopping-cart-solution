543fca4842deb4ff5b40818fee02b4cc15895912
sufian <sufian4se@gmail.com>
2022-08-18 21:14:03 +0600

    increase case price by case number

--- js/case.js
+++ js/case.js
@@ -18,10 +18,16 @@ function updateCaseNumber(isIncrease) {
     newCaseNumber = previousCaseNumber - 1;
   }
   caseNumberField.value = newCaseNumber;
-}
+
+  return newCaseNumber;
+};
 
 document.getElementById("btn-case-plus").addEventListener("click", function () {
-  updateCaseNumber(true);
+  const newCaseNumber = updateCaseNumber(true);
+
+  const caseTotalPrice = newCaseNumber * 59;
+  const caseTotalElement = document.getElementById('case-total');
+  caseTotalElement.innerText = caseTotalPrice;
 });
 
 document
