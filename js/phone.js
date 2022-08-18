215988f03fc517247a68b19d37ed6d424fd7a712
sufian <sufian4se@gmail.com>
2022-08-18 22:17:21 +0600

    set phone plus and minus button working

---
+++ js/phone.js
@@ -0,0 +1,27 @@
+function updatePhoneNumber(isIncrease) {
+  const phoneNumberField = document.getElementById("phone-number-field");
+  const phoneNumberString = phoneNumberField.value;
+  const previousPhoneNumber = parseInt(phoneNumberString);
+
+  let newPhoneNumber;
+  if (isIncrease === true) {
+    newPhoneNumber = previousPhoneNumber + 1;
+  } else {
+    newPhoneNumber = previousPhoneNumber - 1;
+  }
+  phoneNumberField.value = newPhoneNumber;
+
+  return newPhoneNumber;
+}
+
+document
+  .getElementById("btn-phone-plus")
+  .addEventListener("click", function () {
+    updatePhoneNumber(true);
+  });
+
+document
+  .getElementById("btn-phone-minus")
+  .addEventListener("click", function () {
+    updatePhoneNumber(false);
+  });
