10367ceb0b7eb83a4caa3c8e0b6897383fef3670
sufian <sufian4se@gmail.com>
2022-08-18 20:48:20 +0600

    first commit

---
+++ js/case.js
@@ -0,0 +1,26 @@
+/* 
+1. add event listener to the case plus button
+2. get the value inside the case number field (input field)
+3. convert the number to an integer
+4. calculate the new case number
+5. set the value to the case number field
+*/
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+
+    const newCaseNumber = previousCaseNumber + 1;
+    caseNumberField.value = newCaseNumber;
+});
+
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+
+    const newCaseNumber = previousCaseNumber - 1;
+    caseNumberField.value = newCaseNumber;
+});
